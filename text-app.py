7e36a4dc8982084bc22fcabbcb525e42a250b14a
gabriel <gabriel_sapitula@yahoo.com>
2012-08-13 09:18:15 +0800

    text-app

---
+++ text-app.py
@@ -0,0 +1,24 @@
+import tkinter
+
+class Application(tkinter.Frame):
+
+    def __init__(self,master):
+        tkinter.Frame.__init__(self,master)
+        self.pack()
+        self.CreateWidgets()
+
+    def CreateWidgets(self):
+        self.textbox1=tkinter.Text()
+        self.textbox1.pack()
+        self.button1=tkinter.Button()
+        self.button1["command"]=self.button1_click
+        self.button1["text"]="click"
+        self.button1.pack()
+
+    def button1_click(self):
+        pass
+
+root=tkinter.Tk()
+app=Application(master=root)
+app.mainloop()
+root.destroy()
